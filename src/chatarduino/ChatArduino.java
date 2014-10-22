3a3f39d3a59d5c00e05cc2bdd69beca70940567f
haroruhomer <harmol22@hotmail.com>
2014-10-22 08:38:38 -0500

    Corregido
    Este si es

---
+++ src/chatarduino/ChatArduino.java
@@ -0,0 +1,9 @@
+package chatarduino;
+public class ChatArduino {
+public static void main(String[] args) {
+        // TODO code application logic here
+        Chat ventana=new Chat();
+        ventana.setVisible(true);
+    }
+    
+}
--- src/chatarduino/chat.java
+++ src/chatarduino/chat.java
@@ -1,5 +1,162 @@
 package chatarduino;
 
-public interface chat {
+import Arduino.Arduino;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class Chat extends javax.swing.JFrame {
+    Arduino ard=new Arduino();//objeto de tipo arduino
+    String cadena="";//varible tipo string vacia
+    SerialPortEventListener puerto =new SerialPortEventListener() {//listener llamado puerto
+
+    public String limpiacadena(String cad){
+        String cadlimpia; 
+        int i=cad.length();//obtiene el largo de la cadena
+        cadlimpia=cad.substring(0, i/2);//obtiene la mitad de la cadena 
+        //System.out.println(cadlimpia);
+        return cadlimpia;
+    }
+        
+        @Override
+        public void serialEvent(SerialPortEvent spe) {//empieza el listener
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            if(ard.MessageAvailable()){//si hay mensaje disponible en el objeto adruino
+               cadena=limpiacadena(ard.PrintMessage());//envia el mensaje a la cadena limpia y este la envia
+               TA_texto.setText(TA_texto.getText()+"Otro: "+cadena+"\n");//se imprime cadena receptor en el text area
+               
+            }
+        }
+    };
+    public Chat() {
+        initComponents();
+        try {
+            ard.ArduinoRXTX("COM6", 2000, 9600, puerto);//establece la comunicacion(puerto,seg,baudio,listener)
+        } catch (Exception ex) {
+            Logger.getLogger(Chat.class.getName()).log(Level.SEVERE, null, ex);//error
+        }
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        TA_texto = new javax.swing.JTextArea();
+        TF_texto = new javax.swing.JTextField();
+        B_enviar = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        TA_texto.setEditable(false);
+        TA_texto.setColumns(20);
+        TA_texto.setRows(5);
+        jScrollPane1.setViewportView(TA_texto);
+
+        B_enviar.setText("Enviar");
+        B_enviar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                B_enviarActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Limpiar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(TF_texto, javax.swing.GroupLayout.PREFERRED_SIZE, 304, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(B_enviar)
+                        .addGap(0, 10, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(140, 140, 140)
+                .addComponent(jButton1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(TF_texto, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(B_enviar))
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void B_enviarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_B_enviarActionPerformed
+        try {
+            ard.SendData(TF_texto.getText());//envia lo q esta en la caja de texto al arduino
+            TA_texto.setText(TA_texto.getText()+"Yo: "+TF_texto.getText()+"\n");//imprime el mensaje del emisor en el text area
+            TF_texto.setText("");//borra el texto
+            System.out.println("holi");//imprime consola si hay counicacion
+        } catch (Exception ex) {
+            Logger.getLogger(Chat.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_B_enviarActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+            // TODO add your handling code here
+        TA_texto.setText("");//limpiar la pantalla
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Chat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Chat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Chat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Chat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Chat().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton B_enviar;
+    private javax.swing.JTextArea TA_texto;
+    private javax.swing.JTextField TF_texto;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JScrollPane jScrollPane1;
+    // End of variables declaration//GEN-END:variables
 }
